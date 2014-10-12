4ac285c172767b7d0f44e478472190947a5c1e42
Rohan Likhite <rlikhite@gmail.com>
2014-10-11 22:58:30 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,17 +1,4 @@
 InstaBingo
 ==========
 
-An interactive Instagram themed bingo board 
-
-###Changelog
-- Set up Index (2 Col - Board/How To)
-- Added win functionality
-- Got random text to appear on tiles
-- Started working on images
-
-###To-Do
-- Load Images as TD background
-- Add Instagram profile browser
-- Change look (Colours)
-- Logo
-- Fix overlay
+An Instagram themed interactive Bingo board
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 //<!--Rohan Likhite -->
-$(document).ready(function() {
+$(document).ready(function () {
     //Array of strings to be randomized - Bingo Square text
-    var theList = ["POV Legs","Latte Art","#OOTD","ALCOHOL","Throwback","Black and White","Fitness","POV Driving","Food","Quote","Pets","Selfie","Texts","The Crew","Cars","#JobLove","PlaneWing","Concert","Starbucks","Tattoo","#NoFilter","Feet","#TakeMeBack","City Skyline"];
-    theList.sort(function() {
+    var theList = ["POV Legs", "Latte Art", "#OOTD", "ALCOHOL", "Throwback", "Black and White", "Fitness", "POV Driving", "Food", "Quote", "Pets", "Selfie", "Texts", "The Crew", "Cars", "#JobLove", "PlaneWing", "Concert", "Starbucks", "Tattoo", "#NoFilter", "Feet", "#TakeMeBack", "City Skyline"];
+    theList.sort(function () {
         return 0.5 - Math.random();
     });
-    for (var i = 0; i<25; i++) {
-//ID's of table cells are #sq1, #sq2, etc.
-      
-    
-        $("#sq"+(i+1)).append(theList[i]);
-          //newImage = "url(http://i.imgur.com/LsEqAim.jpg)";
+    for (var i = 0; i < 25; i++) {
+        //ID's of table cells are #sq1, #sq2, etc.
+
+
+        $("#sq" + (i + 1)).append(theList[i]);
+        //newImage = "url(http://i.imgur.com/LsEqAim.jpg)";
         //newImage = "url('bg1.jpg')";
         //newImage = 'url(./FoodImages/fruit' + i.toString() + '.jpg)';
         //document.getElementById('sq'+(i+1)).style.backgroundImage = newImage;
     }
-    $('td').click(function() {
+    $('td').click(function () {
         $(this).toggleClass('ui');
         if ($(this).data('value') == 1) {
             $(this).data('value', 0);
@@ -23,22 +23,22 @@ $(document).ready(function() {
             $(this).data('value', 1);
         }
         //Check for bingo
-        var row1 = ($('#sq1').data('value')+$('#sq2').data('value')+$('#sq3').data('value')+$('#sq4').data('value')+$('#sq5').data('value'));
-        var row2 = ($('#sq6').data('value')+$('#sq7').data('value')+$('#sq8').data('value')+$('#sq9').data('value')+$('#sq10').data('value'));
-        var row3 = ($('#sq11').data('value')+$('#sq12').data('value')+$('#free').data('value')+$('#sq13').data('value')+$('#sq14').data('value'));
-        var row4 = ($('#sq15').data('value')+$('#sq16').data('value')+$('#sq17').data('value')+$('#sq18').data('value')+$('#sq19').data('value'));
-        var row5 = ($('#sq20').data('value')+$('#sq21').data('value')+$('#sq22').data('value')+$('#sq23').data('value')+$('#sq24').data('value'));
+        var row1 = ($('#sq1').data('value') + $('#sq2').data('value') + $('#sq3').data('value') + $('#sq4').data('value') + $('#sq5').data('value'));
+        var row2 = ($('#sq6').data('value') + $('#sq7').data('value') + $('#sq8').data('value') + $('#sq9').data('value') + $('#sq10').data('value'));
+        var row3 = ($('#sq11').data('value') + $('#sq12').data('value') + $('#free').data('value') + $('#sq13').data('value') + $('#sq14').data('value'));
+        var row4 = ($('#sq15').data('value') + $('#sq16').data('value') + $('#sq17').data('value') + $('#sq18').data('value') + $('#sq19').data('value'));
+        var row5 = ($('#sq20').data('value') + $('#sq21').data('value') + $('#sq22').data('value') + $('#sq23').data('value') + $('#sq24').data('value'));
 
-        var col1 = ($('#sq1').data('value')+$('#sq6').data('value')+$('#sq11').data('value')+$('#sq15').data('value')+$('#sq20').data('value'));
-        var col2 = ($('#sq2').data('value')+$('#sq7').data('value')+$('#sq12').data('value')+$('#sq16').data('value')+$('#sq21').data('value'));
-        var col3 = ($('#sq3').data('value')+$('#sq8').data('value')+$('#free').data('value')+$('#sq17').data('value')+$('#sq22').data('value'));
-        var col4 = ($('#sq4').data('value')+$('#sq9').data('value')+$('#sq13').data('value')+$('#sq18').data('value')+$('#sq23').data('value'));
-        var col5 = ($('#sq5').data('value')+$('#sq10').data('value')+$('#sq14').data('value')+$('#sq19').data('value')+$('#sq24').data('value'));
+        var col1 = ($('#sq1').data('value') + $('#sq6').data('value') + $('#sq11').data('value') + $('#sq15').data('value') + $('#sq20').data('value'));
+        var col2 = ($('#sq2').data('value') + $('#sq7').data('value') + $('#sq12').data('value') + $('#sq16').data('value') + $('#sq21').data('value'));
+        var col3 = ($('#sq3').data('value') + $('#sq8').data('value') + $('#free').data('value') + $('#sq17').data('value') + $('#sq22').data('value'));
+        var col4 = ($('#sq4').data('value') + $('#sq9').data('value') + $('#sq13').data('value') + $('#sq18').data('value') + $('#sq23').data('value'));
+        var col5 = ($('#sq5').data('value') + $('#sq10').data('value') + $('#sq14').data('value') + $('#sq19').data('value') + $('#sq24').data('value'));
 
-        var diag1 = ($('#sq1').data('value')+$('#sq7').data('value')+$('#free').data('value')+$('#sq18').data('value')+$('#sq24').data('value'));
-        var diag2 = ($('#sq5').data('value')+$('#sq9').data('value')+$('#free').data('value')+$('#sq16').data('value')+$('#sq20').data('value'));
+        var diag1 = ($('#sq1').data('value') + $('#sq7').data('value') + $('#free').data('value') + $('#sq18').data('value') + $('#sq24').data('value'));
+        var diag2 = ($('#sq5').data('value') + $('#sq9').data('value') + $('#free').data('value') + $('#sq16').data('value') + $('#sq20').data('value'));
 
-        if (row1 == 5 || row2 == 5 || row3 == 5 || row4 == 5 || row5 == 5 || col1 == 5 || col2 == 5 || col3 == 5  || col4 == 5  || col5 == 5 || diag1 == 5 || diag2 == 5) {
+        if (row1 == 5 || row2 == 5 || row3 == 5 || row4 == 5 || row5 == 5 || col1 == 5 || col2 == 5 || col3 == 5 || col4 == 5 || col5 == 5 || diag1 == 5 || diag2 == 5) {
             $('#winner').slideDown('fast');
         }
     });
